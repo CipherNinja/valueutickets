469cfcd02848f170fc3c7d8555cfc849e9bd3701
Prashant Jha <jhap5754@gmail.com>
2025-03-03 12:14:35 +0530

    Fixed the layout of filter component and made it responsive

--- front/src/components/FM.vue
+++ front/src/components/FM.vue
@@ -6,102 +6,88 @@
     </button>
 
     <!-- Mobile Filter Menu -->
-    <div v-if="isMobileMenuOpen" class="mobile-filter-menu">
-      <div class="mobile-filter-header">
-        <h5>Filters</h5>
-        <i class="bi bi-x" @click="isMobileMenuOpen = false"></i>
-      </div>
-
-      <div class="mobile-filter-body">
-        <!-- Stops Filter -->
-        <div class="filter-section">
-          <div class="d-flex justify-content-between align-items-center">
-            <p class="filter-title">STOPS</p>
-            <i
-              :class="isStopsOpen ? 'bi bi-chevron-up' : 'bi bi-chevron-down'"
-              class="fs-5"
-              @click="toggleStops"
-            ></i>
-          </div>
-          <div v-if="isStopsOpen">
-            <div class="form-check">
-              <input
-                type="checkbox"
-                id="1stop"
-                v-model="oneStop"
-                class="form-check-input"
-              />
-              <label for="1stop" class="form-check-label">1 Stop</label>
-            </div>
-            <div class="form-check">
-              <input
-                type="checkbox"
-                id="2stop"
-                v-model="twoStops"
-                class="form-check-input"
-              />
-              <label for="2stop" class="form-check-label">2 Stops</label>
-            </div>
-          </div>
+    <transition name="slide">
+      <div v-if="isMobileMenuOpen" class="mobile-filter-menu">
+        <div class="mobile-filter-header">
+          <h5>Filters</h5>
+          <i class="bi bi-x close-icon" @click="isMobileMenuOpen = false"></i>
         </div>
 
-        <!-- Flight Time Filter (Moved Above Pricing) -->
-        <div class="filter-section">
-          <div class="d-flex justify-content-between align-items-center">
-            <p class="filter-title">FLIGHT TIME</p>
-            <i
-              :class="
-                isFlightTimeOpen ? 'bi bi-chevron-up' : 'bi bi-chevron-down'
-              "
-              class="fs-5"
-              @click="toggleFlightTime"
-            ></i>
+        <div class="mobile-filter-body">
+          <!-- Stops Filter -->
+          <div class="filter-section">
+            <p class="filter-title">STOPS</p>
+            <div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  id="dstop"
+                  v-model="oneStop"
+                  class="form-check-input"
+                />
+                <label for="dstop" class="form-check-label">Direct Stops</label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  id="1stop"
+                  v-model="oneStop"
+                  class="form-check-input"
+                />
+                <label for="1stop" class="form-check-label">1 Stop</label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  id="2stop"
+                  v-model="twoStops"
+                  class="form-check-input"
+                />
+                <label for="2stop" class="form-check-label">2 Stops</label>
+              </div>
+            </div>
           </div>
-          <div v-if="isFlightTimeOpen">
-            <ul>
-              <li>3:00 PM</li>
-              <li>4:00 PM</li>
-              <li>5:00 PM</li>
-              <li>6:00 PM</li>
+
+          <!-- Flight Time Filter -->
+          <div class="filter-section">
+            <p class="filter-title">FLIGHT TIME</p>
+            <ul class="flight-time-list">
+              <li v-for="(flight, index) in flightTimes" :key="index">
+                <span class="flight-time-item">
+                  <img
+                    :src="flight.imgSrc"
+                    :alt="flight.time"
+                    class="img-fluid"
+                  />
+                  <p
+                    class="mb-0 mt-2 fw-bold"
+                    style="font-size: 13px; text-wrap: nowrap"
+                  >
+                    {{ flight.time }}
+                  </p>
+                </span>
+              </li>
             </ul>
           </div>
-        </div>
 
-        <!-- Pricing Filter -->
-        <div class="filter-section">
-          <div class="d-flex justify-content-between align-items-center">
+          <!-- Pricing Filter -->
+          <div class="filter-section">
             <p class="filter-title">PRICING</p>
-            <i
-              :class="isPricingOpen ? 'bi bi-chevron-up' : 'bi bi-chevron-down'"
-              class="fs-5"
-              @click="togglePricing"
-            ></i>
-          </div>
-          <div v-if="isPricingOpen">
             <input
               type="range"
               v-model="sliderValue"
               min="0"
               max="100"
               class="form-range"
+              @input="updatePrice"
+              style="width: 100%; height: 4px; accent-color: #3235e3"
             />
             <p>Selected Price: {{ formattedPrice }}</p>
           </div>
-        </div>
 
-        <!-- Airline Filter -->
-        <div class="filter-section">
-          <div class="d-flex justify-content-between align-items-center">
+          <!-- Airline Filter -->
+          <div class="filter-section">
             <p class="filter-title">AIRLINE</p>
-            <i
-              :class="
-                isAirlinesOpen ? 'bi bi-chevron-up' : 'bi bi-chevron-down'
-              "
-              class="fs-5"
-              @click="toggleAirlines"
-            ></i>
-          </div>
-          <div v-if="isAirlinesOpen">
             <div class="form-check">
               <input
                 type="checkbox"
@@ -124,18 +110,18 @@
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- Buttons at the Bottom -->
-      <div class="mobile-filter-footer">
-        <button class="btn btn-primary" @click="applyFilters">
-          Apply Filter
-        </button>
-        <button class="btn btn-secondary" @click="clearFilters">
-          Clear Filter
-        </button>
+        <!-- Buttons at the Bottom -->
+        <div class="mobile-filter-footer">
+          <button class="btn btn-primary" @click="applyFilters">
+            Apply Filter
+          </button>
+          <button class="btn btn-secondary" @click="clearFilters">
+            Clear Filter
+          </button>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -150,15 +136,17 @@ export default {
       twoStops: false,
       americanAirlines: false,
       deltaAirlines: false,
-      isStopsOpen: false,
-      isPricingOpen: false,
-      isAirlinesOpen: false,
-      isFlightTimeOpen: false, // New data property to handle flight time filter toggle
+      flightTimes: [
+        { time: "(05-12)", imgSrc: "/icons8-morning-32.png" },
+        { time: "(12-18)", imgSrc: "/icons8-sun-50.png" },
+        { time: "(18-24)", imgSrc: "/icons8-evening-64.png" },
+        { time: "(00-05)", imgSrc: "/icons8-night-50.png" },
+      ],
     };
   },
   computed: {
     formattedPrice() {
-      const priceIndex = Math.floor((this.sliderValue / 100) * 2); // Adjusted for simplicity in mobile context
+      const priceIndex = Math.floor((this.sliderValue / 100) * 2);
       return `$${[160, 180, 208][priceIndex]}`;
     },
   },
@@ -169,38 +157,20 @@ export default {
       this.americanAirlines = false;
       this.deltaAirlines = false;
       this.sliderValue = 0;
-      this.isMobileMenuOpen = false;
-      this.isStopsOpen = false;
-      this.isPricingOpen = false;
-      this.isAirlinesOpen = false;
-      this.isFlightTimeOpen = false; // Reset the flight time filter state
     },
     applyFilters() {
       this.isMobileMenuOpen = false;
-    },
-    toggleStops() {
-      this.isStopsOpen = !this.isStopsOpen;
-    },
-    togglePricing() {
-      this.isPricingOpen = !this.isPricingOpen;
-    },
-    toggleAirlines() {
-      this.isAirlinesOpen = !this.isAirlinesOpen;
-    },
-    toggleFlightTime() {
-      this.isFlightTimeOpen = !this.isFlightTimeOpen; // Toggle the flight time filter
     },
   },
 };
 </script>
 
 <style scoped>
-/* Mobile Filter Button */
 .mobile-filter-btn {
   position: absolute;
-  top: -5px;
-  left: 20px;
-  background: #71aeffe5;
+  top: 0px;
+  left: 2px;
+  background: #3235e3;
   color: white;
   padding: 10px 15px;
   border: none;
@@ -213,12 +183,11 @@ export default {
   z-index: 1000;
 }
 
-/* Mobile Filter Menu */
 .mobile-filter-menu {
   position: fixed;
   top: 0;
   left: 0;
-  width: 300px;
+  width: 100vw;
   height: 100vh;
   background: white;
   z-index: 1000;
@@ -227,7 +196,6 @@ export default {
   padding: 20px;
 }
 
-/* Header */
 .mobile-filter-header {
   display: flex;
   justify-content: space-between;
@@ -237,18 +205,73 @@ export default {
   margin-bottom: 20px;
 }
 
-.mobile-filter-header i {
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.5s ease-in-out, opacity 0.3s ease-in-out;
+}
+
+.slide-enter-from {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.slide-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.close-icon {
   cursor: pointer;
   font-size: 1.5rem;
 }
 
-/* Filter Body */
 .mobile-filter-body {
   flex-grow: 1;
   overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 }
 
-/* Footer Buttons */
+.mobile-filter-body::-webkit-scrollbar {
+  display: none;
+}
+
+.filter-title {
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.flight-time-list {
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  margin-top: 10px;
+  list-style: none;
+}
+
+.flight-time-item {
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 60px;
+}
+
+.img-fluid {
+  width: 50px;
+  height: auto;
+}
+
+.form-range {
+  height: 10px;
+  background: #3235e3;
+  border-radius: 5px;
+  outline: none;
+}
+
 .mobile-filter-footer {
   display: flex;
   justify-content: space-between;
@@ -259,27 +282,20 @@ export default {
   width: 48%;
 }
 
-/* Filter Title */
-.filter-title {
-  font-weight: bold;
-  font-size: 18px;
+.form-range {
+  -webkit-appearance: none;
+  appearance: none;
+  height: 10px;
+  background: #3235e3;
+  border-radius: 5px;
+  outline: none;
+  transition: background 0.3s ease;
+  color: #3235e3;
 }
 
-/* Flight Time Section */
-.filter-section ul {
-  list-style: none;
-  padding: 0;
-  margin-top: 10px;
-}
-
-.filter-section ul li {
-  font-size: 16px;
-}
-
-/* Expanded Filter Content */
-.filter-content {
-  margin-top: 10px;
-  padding-left: 15px;
+.form-range:active,
+.form-range:focus {
+  background: #3235e3;
 }
 
 @media (min-width: 768px) {
